ac2847c69c20f26a38b348c20c31f5c345fe6b44
Nali1919 <my@email.com>
2022-09-27 17:06:39 +0200

    Creo v-model e metodo

--- src/components/SelectComponent.vue
+++ src/components/SelectComponent.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <select name="" id="">
+    <select name="" id="" v-model="genere" @change="cambioGenere">
         <option value="All">All</option>
         <option value="Jazz">Jazz</option>
         <option value="Metal">Metal</option>
         <option value="Pop">Pop</option>
         <option value="Rock">Rock</option>
-
     </select>
   </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            genere: ''
+        }
+    },
+    methods: {
+        cambioGenere() {
+     console.log(this.genere)
+    }
+},
 name: 'SelectComponent'
-
 }
+
 </script>
 
 <style>
